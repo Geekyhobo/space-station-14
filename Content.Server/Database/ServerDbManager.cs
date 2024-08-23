23652e1e69c68fffb56fc767faba97a937750540
Geekyhobo <geekyhobo321@gmail.com>
2024-08-23 11:38:23 -0400

    nuke ahelp participent

--- Content.Server/Database/ServerDbManager.cs
+++ Content.Server/Database/ServerDbManager.cs
@@ -222,8 +222,6 @@ namespace Content.Server.Database
         Task<AhelpExchange?> GetAhelpExchangeAsync(int ahelpRound, Guid ahelpTarget);
         Task AddAhelpExchangeAsync(AhelpExchange exchange);
         Task AddAhelpMessageAsync(AhelpMessage message);
-        Task<AhelpParticipant?> GetAhelpParticipantAsync(int ahelpId, int playerId);
-        Task AddAhelpParticipantAsync(AhelpParticipant participant);
         Task<int> GetMaxMessageIdForExchange(int ahelpId);
 
         #endregion
@@ -747,18 +745,6 @@ namespace Content.Server.Database
         {
             DbWriteOpsMetric.Inc();
             return RunDbCommand(() => _db.AddAhelpMessageAsync(message));
-        }
-
-        public Task<AhelpParticipant?> GetAhelpParticipantAsync(int ahelpId, int playerId)
-        {
-            DbReadOpsMetric.Inc();
-            return RunDbCommand(() => _db.GetAhelpParticipantAsync(ahelpId, playerId));
-        }
-
-        public Task AddAhelpParticipantAsync(AhelpParticipant participant)
-        {
-            DbWriteOpsMetric.Inc();
-            return RunDbCommand(() => _db.AddAhelpParticipantAsync(participant));
         }
 
         public Task<int> GetMaxMessageIdForExchange(int ahelpId)
