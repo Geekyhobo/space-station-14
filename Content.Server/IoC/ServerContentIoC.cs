The-Hullrot-Development-Team/Hullrot

Address some reviews

Content.Server/IoC/ServerContentIoC.cs
@@ -73,7 +73,7 @@ namespace Content.Server.IoC
             IoCManager.Register<PlayerRateLimitManager>();
             IoCManager.Register<SharedPlayerRateLimitManager, PlayerRateLimitManager>();
             IoCManager.Register<MappingManager>();
-            IoCManager.Register<SupportExchangeLogging, SupportExchangeLogging>();
+            IoCManager.Register<SupportExchangeLogging>();
         }
     }
 }

Geekyhobo/space-station-14

Content.Server/IoC/ServerContentIoC.cs
@@ -85,5 +85,6 @@ internal static class ServerContentIoC
         deps.Register<ServerFeedbackManager>();
         deps.Register<ISharedFeedbackManager, ServerFeedbackManager>();
         deps.Register<AhelpLogging, AhelpLogging>();
+        deps.Register<SupportExchangeLogging>();
     }
 }