lzk228/space-axolotl-14

Very good, baseline logging of a single ahelp works and has been tested here, going to probably remove ahelp partipent bc its redudent. need to test other test cases

Content.Server/IoC/ServerContentIoC.cs
@@ -68,6 +68,7 @@ namespace Content.Server.IoC
             IoCManager.Register<JobWhitelistManager>();
             IoCManager.Register<PlayerRateLimitManager>();
             IoCManager.Register<MappingManager>();
+            IoCManager.Register<AhelpLogging, AhelpLogging>();
         }
     }
 }

Geekyhobo/space-station-14

Content.Server/IoC/ServerContentIoC.cs
@@ -84,5 +84,6 @@ internal static class ServerContentIoC
         deps.Register<DiscordChatLink>();
         deps.Register<ServerFeedbackManager>();
         deps.Register<ISharedFeedbackManager, ServerFeedbackManager>();
+        deps.Register<AhelpLogging, AhelpLogging>();
     }
 }